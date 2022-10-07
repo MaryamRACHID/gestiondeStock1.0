b779f431d7c24798d3663ddf143a20281f7b909e
Maryam RACHID <maryamrachidgi@gmail.com>
2022-10-07 23:36:06 +0100

    Commit 1.0

---
+++ src/main/java/com/app/gestiondestock/GestiondeStockApplication.java
@@ -0,0 +1,13 @@
+package com.app.gestiondestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondeStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestiondeStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/gestiondestock/GestiondeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gestiondestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondeStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
